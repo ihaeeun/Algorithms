0cd6798e6ce04ca9ac42d4018afece17de7fef3f
ihaeeun <ihaeeun16@gmail.com>
2019-04-11 19:33:58 +0900

    2018 서머코딩 - 예산

---
+++ Budget/Main.java
@@ -0,0 +1,12 @@
+package Budget;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+
+        int[] d = {1, 3, 2, 5, 4};
+        int budget = 9;
+
+        System.out.println(solution.solution(d, budget));
+    }
+}
