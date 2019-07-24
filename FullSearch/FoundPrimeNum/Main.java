72e0bbfa606292c7dc0a205d81caf293c15aa9c2
ihaeeun <ihaeeun16@gmail.com>
2019-07-24 17:06:40 +0900

    정리

--- FullSearch/FoundPrimeNum/Main.java
+++
@@ -1,9 +0,0 @@
-package FullSearch.FoundPrimeNum;
-
-public class Main {
-    public static void main(String[] args) {
-        String numbers = "17";
-        Solution solution = new Solution();
-        System.out.println(solution.solution(numbers));
-    }
-}
--- StackQueue/IronBar/Main.java
+++
@@ -1,9 +0,0 @@
-package StackQueue.IronBar;
-
-public class Main {
-    public static void main(String[] args) {
-        Solution solution = new Solution();
-        String arrangement = "()(((()())(())()))(())";
-        System.out.println(solution.solution(arrangement));
-    }
-}
